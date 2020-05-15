c3dd56e5da982c97d5b27bc1fbf25025ec1e7a7a
IsaacMaciel <isaac_maciel@hotmail.com>
2020-05-15 16:04:42 -0300

    Revert "criacao das telas de login e de esqueci senha"
    This reverts commit 6f97e8fb9dc1e6664e27cc6545ef7e2f3a233ea7.

--- src/app/controllers/sessionController.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    index(req,res) {
-        return res.render("session/login");
-    },
-    forgot(req,res) {
-        return res.render("session/forgot-password");
-    }
-}
